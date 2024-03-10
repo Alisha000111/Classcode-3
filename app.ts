eb5cff5db529716ba9356436976f404217a4cb7b
Alisha M Irfan <160208713+Alisha000111@users.noreply.github.com>
2024-03-10 16:08:54 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,27 @@
+// operators
+// arithmetic operators
+// addition
+// let number1 = 10
+// let number2 = 30
+// console.log(number1 +" "+ number2)
+// subraction
+// let number1 = 0
+// let number2 = 2
+// console.log(number1 - number2)
+// multiplication
+// let number1 = 2
+// let number2 = 10 
+// console.log(number1 * number2)
+// divison
+// let number1 = 5
+// let number2 = 10
+// console.log(number1 /  number2)
+// assingment operators
+let b = 5
+console.log(b === 5) //true
+console.log(b != 5) //false
+console.log(b < 5 ) //false
+console.log(b > 5) //true
+ 
+
+
